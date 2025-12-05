b7734f6f755fbd83d69d71d484d5fd2eb00a0d43
Maninder Singh <maninder.singh@outlook.in>
2025-12-05 15:08:03 -0800

    fix: format supabase_constants.dart

--- lib/core/constants/supabase_constants.dart
+++ lib/core/constants/supabase_constants.dart
@@ -2,5 +2,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class SupabaseConstants {
   static String get supabaseUrl => dotenv.env['NEXT_PUBLIC_SUPABASE_URL'] ?? '';
-  static String get supabaseAnonKey => dotenv.env['NEXT_PUBLIC_SUPABASE_ANON_KEY'] ?? '';
+  static String get supabaseAnonKey =>
+      dotenv.env['NEXT_PUBLIC_SUPABASE_ANON_KEY'] ?? '';
 }
